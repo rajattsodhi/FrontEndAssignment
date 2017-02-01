b27674482568a18717bae380fc18a4c97743f5fe
rajattsodhi <p14166609@my365.dmu.ac.uk>
2017-02-01 22:12:43 +0000

    smooth scrolling testing timer

--- js/smoothscrolling.js
+++ js/smoothscrolling.js
@@ -1,15 +1,15 @@
 
 //http://codereview.stackexchange.com/questions/13111/smooth-page-scrolling-in-javascript
-smoothScrollTo = function () {
-  var timer, start, factor;
+smoothScrollTo = function (target, duration) {
+  //var timer, start, factor;
   
-  return function (target, duration) {
-    var offset = window.pageYOffset,
+    var timer, start, factor;
+      var offset = window.pageYOffset,
         delta  = target - window.pageYOffset; // Y-offset difference
     duration = duration || 1000;              // default 1 sec animation
     start = Date.now();                       // get start time
     factor = 0;
-    
+    console.log(timer);
     if( timer ) {
       clearInterval(timer); // stop any running animations
     }
@@ -17,6 +17,7 @@ smoothScrollTo = function () {
     function step() {
       var y;
       factor = (Date.now() - start) / duration; // get interpolation factor
+      console.log(factor);
       if( factor >= 1 ) {
         clearInterval(timer); // stop animation
         factor = 1;           // clip to max 1.0
@@ -28,7 +29,7 @@ smoothScrollTo = function () {
     timer = setInterval(step, 10);
     return timer;
   };
-}();
+
 
 
 document.getElementById("portfolio-link").onclick = function() {
