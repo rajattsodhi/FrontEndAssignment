a05cf8d7473740c591d919b16bbba2a6060d6747
rajattsodhi <p14166609@my365.dmu.ac.uk>
2017-02-20 22:34:55 +0000

    started doing animated progress bars for languages

---
+++ js/progress-bar.js
@@ -0,0 +1,86 @@
+function move() {
+    var mainDiv = getClosest(event.target, "div");
+    var myProgress = mainDiv.querySelector('.myProgress');
+    if (myProgress.currentStyle) {
+        var displayStyle = myProgress.currentStyle.display;
+        } else if (window.getComputedStyle) {
+        var displayStyle = window.getComputedStyle(myProgress, null).getPropertyValue("display");
+        }
+    if(displayStyle == 'none'){
+        myProgress.style.display="block";
+        var myBar = myProgress.querySelector('.myBar');   
+        var width = 10;
+        var id = setInterval(frame, 10);
+        function frame() {
+            if (width >= 70) {
+            clearInterval(id);
+            } else {
+            width++; 
+            myBar.style.width = width + '%'; 
+            myProgress.querySelector('.label').innerHTML = width * 1  + '%';
+            }
+        }
+    }
+    else {
+        myProgress.style.display="none";
+    }
+  
+}
+
+var languages = document.getElementsByClassName('language');
+console.log(languages);
+//Add listeners
+for (var i = 0; i < languages.length; i++) {
+    var item = languages[i];
+    item.addEventListener("click", function() {
+    move();
+    });
+}
+
+
+/**
+ * Get closest DOM element up the tree that contains a class, ID, or data attribute
+ * @param  {Node} elem The base element
+ * @param  {String} selector The class, id, data attribute, or tag to look for
+ * @return {Node} Null if no match
+ */
+var getClosest = function (elem, selector) {
+
+    var firstChar = selector.charAt(0);
+
+    // Get closest match
+    for ( ; elem && elem !== document; elem = elem.parentNode ) {
+        // If selector is a class
+        if ( firstChar === '.' ) {
+            if ( elem.classList.contains( selector.substr(1) ) ) {
+                return elem;
+            }
+        }
+
+        // If selector is an ID
+        if ( firstChar === '#' ) {
+            if ( elem.id === selector.substr(1) ) {
+                return elem;
+            }
+        } 
+
+        // If selector is a data attribute
+        if ( firstChar === '[' ) {
+            if ( elem.hasAttribute( selector.substr(1, selector.length - 2) ) ) {
+                return elem;
+            }
+        }
+
+        // If selector is a tag
+        if ( elem.tagName.toLowerCase() === selector ) {
+            return elem;
+        }
+
+    }
+
+    return false;
+
+};
+
+
+
